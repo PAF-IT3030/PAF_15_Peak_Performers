92b575e54d6d63eea23f461fdba174d804f36b23
M.Thanusan <99783883+it21175848@users.noreply.github.com>
2024-05-02 16:28:05 +0530

    Delete src directory

--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const API_BASE_URL = "http://localhost:8088";
-export const ACCESS_TOKEN = "token";
-
-export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
-
-export const GOOGLE_AUTH_URL =
-    API_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
-
