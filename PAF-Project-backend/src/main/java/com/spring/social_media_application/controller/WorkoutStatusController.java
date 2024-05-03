775ac239bce4d30a7111ae21488d06e8ef4a161a
it21175770 <99715584+it21175770@users.noreply.github.com>
2024-05-03 22:19:13 +0530

    Update WorkoutStatusController.java
    xzc

--- PAF-Project-backend/src/main/java/com/spring/social_media_application/controller/WorkoutStatusController.java
+++ PAF-Project-backend/src/main/java/com/spring/social_media_application/controller/WorkoutStatusController.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponhfhjfjhjyfhnseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
